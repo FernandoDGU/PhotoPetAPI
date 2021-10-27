2c578c3267c6e38d548281318834ab31900331c1
DarienMSA <sadarien@gmail.com>
2021-10-27 18:49:04 -0500

    CRUD User

---
+++ models/userModel.php
@@ -0,0 +1,194 @@
+<?php
+class User{
+    private $conn;
+    private $table = 'user';
+
+    public $email;
+    public $name;
+    public $password;
+    public $phone;
+    public $description;
+    public $image;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function read(){
+        $query = 'SELECT 
+                    email,
+                    name,
+                    password,
+                    phone,
+                    description,
+                    image
+                  FROM 
+                    ' . $this->table . '  
+                ';
+                
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function singleRead($id){
+        $query = 'SELECT 
+                    email,
+                    name,
+                    password,
+                    phone,
+                    description,
+                    image
+                  FROM 
+                    ' . $this->table . '  
+                  WHERE
+                    email = "' . $id . '"
+                    ';
+                
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        if($num > 0){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->email = $row['email'];
+        $this->name = $row['name'];
+        $this->password = $row['password'];
+        $this->phone = $row['phone'];
+        $this->description = $row['description'];
+        $this->image = "data:image/png;base64," . base64_encode($row['image']);
+        }
+    }
+
+    public function userLogged($email, $pass){
+        $query = 'SELECT 
+                    email,
+                    name,
+                    password,
+                    phone,
+                    description,
+                    image
+                  FROM 
+                    ' . $this->table . '  
+                  WHERE
+                    email = "' . $email . '"
+                    AND
+                    password = "' . $pass . '"';
+                
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        if($num > 0){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->email = $row['email'];
+        $this->name = $row['name'];
+        $this->password = $row['password'];
+        $this->phone = $row['phone'];
+        $this->description = $row['description'];
+        $this->image = "data:image/png;base64," . base64_encode($row['image']);
+        }
+    }
+
+    public function insertUser(){
+        $query = 'INSERT INTO ' . 
+                $this->table . '
+                SET
+                email = :email,
+                name = :name,
+                password = :password,
+                phone = :phone,
+                description = :description,
+                image = :image
+        ';
+        $stmt = $this->conn->prepare($query);
+
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->phone = htmlspecialchars(strip_tags($this->phone));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        
+		//list($type, $this->image) = explode(';', $this->image);
+		//list(, $this->image)      = explode(',', $this->image);
+		//$this->image = base64_decode($this->image);
+
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':password', $this->password);
+        $stmt->bindParam(':phone', $this->phone);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':image', $this->image);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
+    public function updateUser(){
+        $query = 'UPDATE ' . 
+                $this->table . '
+                SET
+                name = :name,
+                password = :password,
+                phone = :phone,
+                description = :description,
+                image = :image
+                WHERE email = :email
+        ';
+        $stmt = $this->conn->prepare($query);
+
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->phone = htmlspecialchars(strip_tags($this->phone));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        
+		//list($type, $this->image) = explode(';', $this->image);
+		//list(, $this->image)      = explode(',', $this->image);
+		//$this->image = base64_decode($this->image);
+
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':password', $this->password);
+        $stmt->bindParam(':phone', $this->phone);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':image', $this->image);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
+    public function deleteUser(){
+        $query = 'DELETE FROM ' . 
+                $this->table . '
+                WHERE email = :email
+        ';
+        $stmt = $this->conn->prepare($query);
+
+        $this->email = htmlspecialchars(strip_tags($this->email));
+
+        $stmt->bindParam(':email', $this->email);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+}
---
+++ user.php
@@ -0,0 +1,185 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once 'database.php';
+include_once 'models/userModel.php';
+
+ switch($_SERVER['REQUEST_METHOD']){
+
+    case 'POST':
+        $data = json_decode(file_get_contents("php://input"));
+        insertUser($data);
+        break;
+
+    case 'GET':
+        if(isset($_GET['email']) && isset($_GET['pass']))
+            logInUser($_GET['email'], $_GET['pass']);
+        else if(isset($_GET['email']))
+            getUserByEmail($_GET['email']);
+        else
+            getUsers();
+        break;
+    
+    case 'PUT':
+        if(isset($_GET['email']))
+        {
+            $data = json_decode(file_get_contents("php://input"));
+            updateUser($data, $_GET['email']);
+        }
+        break;
+
+    case 'DELETE':
+        if(isset($_GET['email']))
+        {
+            deleteUser($_GET['email']);
+        }
+        break;
+
+        
+
+ }
+
+function getUsers(){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $result = $user->read();
+    $num = $result->rowCount();
+   
+    if($num > 0){
+       $users_arr = array();
+       //$users_arr['data'] = array();
+   
+       while($row = $result->fetch(PDO::FETCH_ASSOC)){
+           extract($row);
+   
+           $user_item = array(
+               'email'         => $email,
+               'name'          => $name,
+               'password'      => $password,
+               'phone'         => $phone,
+               'description'   => $description,
+               'image'         => "data:image/png;base64," . base64_encode($image)
+           );
+   
+           //array_push($users_arr['data'], $user_item);
+           array_push($users_arr, $user_item);
+       }
+   
+       echo json_encode($users_arr);
+   
+    } else {
+       echo json_encode(
+           array('message' => 'No Users Found')
+       );
+    }
+}
+
+function getUserByEmail($id){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $result = $user->singleRead($id);
+    if(empty($user->email)){
+        echo json_encode(
+            array('message' => 'No User Found')
+        );
+    }else{
+        $user_arr = array(
+            'email'         => $user->email,
+            'name'          => $user->name,
+            'password'      => $user->password,
+            'phone'         => $user->phone,
+            'description'   => $user->description,
+            'image'         => $user->image
+        );
+        echo json_encode($user_arr);
+    }
+    
+    
+}
+
+function logInUser($email, $pass){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $result = $user->userLogged($email, $pass);
+    if(empty($user->email)){
+        echo json_encode(
+            array('message' => 'No User Found')
+        );
+    }else{
+        $user_arr = array(
+            'email'         => $user->email,
+            'name'          => $user->name,
+            'password'      => $user->password,
+            'phone'         => $user->phone,
+            'description'   => $user->description,
+            'image'         => $user->image
+        );
+        echo json_encode($user_arr);
+    }
+}
+
+function insertUser($data){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $user->email = $data->email;
+    $user->name = $data->name;
+    $user->password = $data->password;
+    $user->phone = $data->phone;
+    $user->description = $data->description;
+    $user->image = $data->image;
+
+    if($user->insertUser()){
+        echo json_encode(
+            array('message' => 'User Created')
+        );
+    }else{
+        echo json_encode(
+            array('message' => 'User Not Created')
+        );
+    }
+}
+
+function updateUser($data, $filterEmail){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $user->email = $filterEmail;
+    $user->name = $data->name;
+    $user->password = $data->password;
+    $user->phone = $data->phone;
+    $user->description = $data->description;
+    $user->image = $data->image;
+
+    if($user->updateUser()){
+        echo json_encode(
+            array('message' => 'User Modificated')
+        );
+    }else{
+        echo json_encode(
+            array('message' => 'User Not Modificated')
+        );
+    }
+}
+
+function deleteUser($filterEmail){
+    $database = new Database();
+    $db = $database->connect();
+    $user = new User($db);
+    $user->email = $filterEmail;
+
+    if($user->deleteUser()){
+        echo json_encode(
+            array('message' => 'User Eliminated')
+        );
+    }else{
+        echo json_encode(
+            array('message' => 'User Not Eliminated')
+        );
+    }
+}
